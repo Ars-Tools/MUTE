732088fcfac0240a7d2df5ac934932f508349922
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-08-13 09:28:04 +0900

    define C module

---
+++ VSP/Sources/module.h
@@ -0,0 +1,24 @@
+//
+//  module.h
+//  MUTE
+//
+//  Created by Kota on 10/11/R6.
+//
+#include"comparison.h"
+#include"logical.h"
+#include"folding.h"
+#include"edge.h"
+#include"spline.h"
+#include"calculus.h"
+#include"random_number_generator.h"
+#include"sinosc_bundle.h"
+#include"biquad_filter.h"
+#include"universal_filter.h"
+#include"state_variable_filter.h"
+#include"median_filter.h"
+#include"periodic_lookup.h"
+#include"vforce+.h"
+#include"utility.h"
+//#ifdef __OBJC__
+//#include"AUParameterNode+.h"
+//#endif
