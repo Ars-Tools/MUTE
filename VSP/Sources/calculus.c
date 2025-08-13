71256322d730a88973f7d2d3d65f26edfb1d2a07
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-08-13 09:27:13 +0900

    add calculus

---
+++ VSP/Sources/calculus.c
@@ -0,0 +1,35 @@
+//
+//  calculus.c
+//  MUTE
+//
+//  Created by Kota on 11/22/R6.
+//
+#include"calculus.h"
+#include <stdio.h>
+void calculus_integration(register double const * __nonnull x,
+						  register double const * __nonnull y, // reset
+						  register double       * __nonnull z,
+						  simd_double2 * __nonnull const w,
+						  intptr_t const length) {
+	register simd_double2 s = *w;
+	for ( register double const * __nonnull const _ = z + length ; z < _ ; ++ x, ++ y, ++ z ) {
+		*z = (s = (simd_double2 const) {
+			*x,
+			(*y<=0) * simd_dot((simd_double3 const){0.5, 1, 0.5}, (simd_double3 const){s.x, s.y, *x})
+		}).y;
+	}
+	*w = s;
+}
+void calculus_differentiation(register double const * __nonnull x,
+							  register double const * __nonnull y, // reset
+							  register double       * __nonnull z,
+							  simd_double2 * __nonnull const w,
+							  intptr_t const length) {
+	register simd_double2 s = *w;
+	for ( register double const * __nonnull const _ = z + length ; z < _ ; ++ x, ++ y, ++ z )
+		*z = (s = (simd_double2 const) {
+			*x,
+			simd_dot((simd_double3 const){-2, (0<*y) - 1, 2}, (simd_double3 const){s.x, s.y, *x}),
+		}).y;
+	*w = s;
+}
