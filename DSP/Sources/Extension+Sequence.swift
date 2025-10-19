6df16f43f90f3176e00b76d81d28409f38974983
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-10-19 20:20:43 +0900

    Sequence extension

---
+++ DSP/Sources/Extension+Sequence.swift
@@ -0,0 +1,34 @@
+//
+//  Extension+Sequence.swift
+//  MUTE
+//
+//  Created by Kota on 7/10/R7.
+//
+@preconcurrency import protocol Combine.Publisher
+@preconcurrency import typealias Combine.Publishers
+import func Layout.concat
+extension Sequence {
+	@inlinable @inline(__always)
+	func roll(head count: Int) -> some Sequence<Element> {
+		concat(dropFirst(count), prefix(count))
+	}
+	@inlinable @inline(__always)
+	func roll(tail count: Int) -> some Sequence<Element> {
+		concat(suffix(count), dropLast(count))
+	}
+	@inlinable @inline(__always)
+	func prefix(count: Int) -> some Publisher<(Int, Element), Never> & Sendable {
+		prefix(count).enumerated().publisher.map(\.self)
+	}
+}
+extension Sequence where Element: Numeric {
+	@inlinable @inline(__always)
+	public var cumulative: some Sequence<Element> {
+		sequence(state: (makeIterator(), Element.zero)) { state in
+			state.0.next().map { element in
+				defer { state.1 += element }
+				return state.1
+			}
+		}
+	}
+}
