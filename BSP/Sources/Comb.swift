48fda15fc0a9198b4b2458db6fd16587639bba5a
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-10-19 21:46:39 +0900

    comb filter and allpass filter

---
+++ BSP/Sources/Comb.swift
@@ -0,0 +1,69 @@
+//
+//  Comb.swift
+//  MUTE
+//
+//  Created by Kota on 10/15/25.
+//
+@preconcurrency import protocol Combine.Publisher
+@preconcurrency import typealias Combine.Publishers
+@preconcurrency import typealias Combine.Just
+import typealias CoreMedia.CMTime
+import DSP
+import NSP
+import typealias Synchronization.Atomic
+@usableFromInline
+enum Echo {
+    @usableFromInline
+    struct Kr<Control: Publisher<(DSP.Duration, Float64), Never> & Sendable> {
+        @usableFromInline let source: DSP.Stream
+        @usableFromInline let period: DSP.Duration
+        @usableFromInline let object: Control
+    }
+}
+extension Echo.Kr: DSP.Stream {
+    @inlinable
+    var count: Int {
+        source.count
+    }
+    @inlinable
+    func callAsFunction(interval: CMTime, capacity: Int, instance: inout Instance) throws -> @Sendable (CMTime, Int, UnsafeMutablePointer<Float64>, Int) -> Void {
+        let kernel = try source(interval: interval, capacity: capacity, instance: &instance)
+        let buffer = Buffer(stream: count, period: capacity + period.samples(for: interval))
+        let offset = Atomic<Int>(0)
+        let weight = Atomic<Float64>(0)
+        let cancel = object.sink {
+            offset.store($0.samples(for: interval), ordering: .releasing)
+            weight.store($1, ordering: .releasing)
+        }
+        return {
+            kernel($0, $1, $2, $3)
+            let cursor = $0.samples(for: interval)
+            let (weight, offset) = withExtendedLifetime(cancel) {(
+                weight.load(ordering: .acquiring),
+                offset.load(ordering: .acquiring)
+            )}
+            for stream in 0..<buffer.stream {
+                periodic_lookup_with_update($2.advanced(by: stream * $3),
+                                            $2.advanced(by: stream * $3),
+                                            buffer.start.advanced(by: stream * buffer.period),
+                                            withUnsafePointer(to: weight, \.self), 0,
+                                            cursor,
+                                            offset,
+                                            buffer.period,
+                                            $1)
+            }
+        }
+    }
+}
+public func filter(_ source: DSP.Stream, cmb object: (lag: some Publisher<DSP.Duration, Never>, gain: some Publisher<Float64, Never>), period: DSP.Duration) -> some DSP.Stream {
+    Echo.Kr(source: source, period: period, object: Publishers.Zip(object.0, object.1))
+}
+public func filter(_ source: DSP.Stream, apf object: (lag: some Publisher<DSP.Duration, Never>, gain: some Publisher<Float64, Never>), period: DSP.Duration) -> some DSP.Stream {
+    filter(source, cmb: object, period: period) * object.1.map { -fma($0, $0, -1) } - source
+}
+public func filter(_ source: DSP.Stream, cmb object: (lag: DSP.Duration, gain: Float64)) -> some DSP.Stream {
+    filter(source, cmb: (Just(object.0), Just(object.1)), period: object.0)
+}
+public func filter(_ source: DSP.Stream, apf object: (lag: DSP.Duration, gain: Float64)) -> some DSP.Stream {
+    filter(source, apf: (Just(object.0), Just(object.1)), period: object.0)
+}
