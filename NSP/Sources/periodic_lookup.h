96fc4201347df6f1c794044971280e8de136bd63
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-10-19 18:53:59 +0900

    periodic table referencer

---
+++ NSP/Sources/periodic_lookup.h
@@ -0,0 +1,32 @@
+//
+//  lookup.h
+//  MUTE
+//
+//  Created by Kota on 10/17/R6.
+//
+#include<stdint.h>
+void periodic_lookup_with_static(double const * __nonnull const x,
+								 double const * __nonnull y,
+								 double       * __nonnull z,
+								 intptr_t const period,
+								 intptr_t const length);
+void periodic_lookup_with_active(double const * __nonnull x, intptr_t const ldx,
+								 double const * __nonnull const y,
+								 double       * __nonnull z,
+								 intptr_t const period,
+								 intptr_t const length);
+void periodic_lookup_with_offset(double const * __nonnull const x,
+								 double const * __nonnull const y,
+								 double       * __nonnull z,
+								 double const factor,
+								 intptr_t const cursor, intptr_t const ground,
+								 intptr_t const period,
+								 intptr_t const length);
+void periodic_lookup_with_update(double const * __nonnull const,
+                                 double       * __nonnull const,
+                                 double       * __nonnull const,
+                                 double const * __nonnull const, intptr_t const,
+                                 intptr_t      ,
+                                 intptr_t const,
+                                 intptr_t const,
+                                 intptr_t const);
