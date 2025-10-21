c7aa4a729ba13a0ee48d4e671024db2d18cbdc3a
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-10-21 12:33:23 +0900

    add fade in&out

--- BSP/Sources/Buffer+.swift
+++ BSP/Sources/Buffer+.swift
@@ -6,14 +6,16 @@
 //
 import Accelerate
 import DSP
+@_exported import typealias DSP.Buffer
 extension Buffer {
     @inlinable@_transparent
     public func maximize(dB: Float64 = 0) {
         let signal = zip(stride(from: start, to: start.advanced(by: stream * period), by: period), sequence(first: period, next: \.self)).map(UnsafeMutableBufferPointer.init(start:count:))
         switch vDSP.maximum(signal.map(vDSP.maximumMagnitude)) {
         case let normal where normal.isNormal:
+            let factor = normal * __exp10(-0.05 * dB)
             for signal in signal {
-                vDSP.divide(signal, normal, result: &signal[0..<signal.count])
+                vDSP.divide(signal, factor, result: &signal[0..<signal.count])
             }
         default:
             break
@@ -31,88 +33,51 @@ extension Buffer {
 }
 extension Buffer {
     @inlinable@_transparent
-    static func stretch(from source: Buffer,
-                        to target: inout Buffer,
-                        log2n: Int = 12) {
-        let ratio = Float64(source.period) / Float64(target.period)
-        let frame = 1 << ( log2n - 0 )
-        let shift = 1 << ( log2n - 8 )
-        let count = Swift.max(source.stream, target.stream)
-        let dft = vDSP_create_fftsetupD(.init(log2n), .init(kFFTRadix2)).unsafelyUnwrapped
-        defer { vDSP_destroy_fftsetupD(dft) }
-        target.flush(cursor: 0, length: target.period)
-        withUnsafeTemporaryAllocation(of: Float64.self, capacity: ( 3 * count + 5 ) * frame) {
-            var x = DSPDoubleSplitComplex(realp: $0.baseAddress.unsafelyUnwrapped.advanced(by: ( 0 * count + 0 ) * frame),
-                                          imagp: $0.baseAddress.unsafelyUnwrapped.advanced(by: ( 1 * count + 0 ) * frame))
-            var y = DSPDoubleSplitComplex(realp: $0.baseAddress.unsafelyUnwrapped.advanced(by: ( 3 * count + 0 ) * frame),
-                                          imagp: $0.baseAddress.unsafelyUnwrapped.advanced(by: ( 3 * count + 2 ) * frame))
-            let z = $0.baseAddress.unsafelyUnwrapped.advanced(by: ( 2 * count + 0 ) * frame)
-            let w = $0.baseAddress.unsafelyUnwrapped.advanced(by: ( 3 * count + 4 ) * frame)
-            
-            // window
-            vDSP_vclrD(w, 1, .init(frame))
-            vDSP_hann_windowD(w, .init(frame / 2), .init(vDSP_HANN_DENORM))
-            
-            for cursor in stride(from: 0, to: target.period - frame, by: shift) {
-                
-                // fetch
-                let convey = Int(ratio * Float64(cursor))
-                
-                // radius
-                source.copy(cursor: convey, length: frame, target: x.realp, stride: frame)
-                for r in stride(from: x.realp, to: x.realp.advanced(by: count * frame), by: frame) {
-                    vDSP_vmulD(r, 1, w, 1, r, 1, .init(frame))
-                }
-                vDSP_vclrD(x.imagp, 1, .init(count * frame))
-                vDSP_fftm_ziptD(dft,
-                                &x, 1, frame,
-                                &y,
-                                .init(log2n), .init(count),
-                                .init(kFFTDirection_Forward))
-                vDSP_zvabsD(&x, 1, z, 1, .init(count * frame))
-                
-                // radian
-                target.copy(cursor: cursor, length: frame, target: x.realp, stride: frame)
-                for r in stride(from: x.realp, to: x.realp.advanced(by: count * frame), by: frame) {
-                    vDSP_vmulD(r, 1, w, 1, r, 1, .init(frame))
-                }
-                vDSP_vclrD(x.imagp, 1, .init(count * frame))
-                vDSP_fftm_ziptD(dft,
-                                &x, 1, frame,
-                                &y,
-                                .init(log2n), .init(count),
-                                .init(kFFTDirection_Forward))
-                vDSP_zvphasD(&x, 1, x.imagp, 1, .init(count * frame))
-                
-                // synth
-                vvsincos(x.imagp, x.realp, x.imagp, Swift.withUnsafePointer(to: Int32(count * frame), \.self))
-                vDSP_vmulD(z, 1, x.realp, 1, x.realp, 1, .init(count * frame))
-                vDSP_vmulD(z, 1, x.imagp, 1, x.imagp, 1, .init(count * frame))
-                vDSP_fftm_ziptD(dft,
-                                &x, 1, frame,
-                                &y,
-                                .init(log2n), .init(count),
-                                .init(kFFTDirection_Inverse))
-                vDSP_vsdivD(x.realp, 1,
-                            Swift.withUnsafePointer(to: Float64(frame), \.self),
-                            x.realp, 1,
-                            .init(count * frame))
-                target.blend(cursor: cursor, length: frame, weight: w, source: x.realp, stride: frame)
-            }
+    func fade(prefix ramp: Array<Float64>) {
+        let signal = zip(stride(from: start, to: start.advanced(by: stream * period), by: period), sequence(first: period, next: \.self)).map(UnsafeMutableBufferPointer.init(start:count:))
+        let phasor = ramp.suffix(period)
+        for signal in signal {
+            vDSP.multiply(phasor, signal[0..<phasor.count], result: &signal[0..<phasor.count])
         }
     }
-    public func stretch(ratio: Float64, log2n: Int = 12) -> Buffer {
-        var target = Buffer(stream: stream, period: .init(Float64(period) * ratio) + 1 << log2n)
-        Buffer.stretch(from: self, to: &target, log2n: log2n)
-        return target
-    }
-    public func stretch(to target: inout Buffer, log2n: Int = 12) {
-        Buffer.stretch(from: self, to: &target, log2n: log2n)
-    }
-}
-extension Buffer {
     @inlinable@_transparent
-    func resample(to: inout Buffer) {
-        
+    func fade(suffix ramp: Array<Float64>) {
+        let signal = zip(stride(from: start, to: start.advanced(by: stream * period), by: period), sequence(first: period, next: \.self)).map(UnsafeMutableBufferPointer.init(start:count:))
+        let phasor = ramp.prefix(period)
+        for signal in signal {
+            vDSP.multiply(phasor, signal[period-phasor.count..<period], result: &signal[period-phasor.count..<period])
+        }
+    }
+    @inlinable
+    public func fade(in sample: Int) {
+        fade(prefix: .init(unsafeUninitializedCapacity: sample) {
+            vDSP.formRamp(withInitialValue: 0, increment: 1, result: &$0)
+            vDSP.divide($0, .init($0.count), result: &$0)
+            $1 = $0.count
+        })
+    }
+    @inlinable
+    public func fade(in sample: Int, dB: Float64) {
+        fade(prefix: .init(unsafeUninitializedCapacity: sample) {
+            vDSP.formRamp(from: 0.05 * M_LOG2E * M_LN10 * dB, through: 0, result: &$0)
+            vForce.exp2($0, result: &$0)
+            $1 = $0.count
+        })
+    }
+    @inlinable
+    public func fade(out sample: Int) {
+        fade(suffix: .init(unsafeUninitializedCapacity: sample) {
+            vDSP.formRamp(withInitialValue: .init($0.count), increment: -1, result: &$0)
+            vDSP.divide($0, .init($0.count), result: &$0)
+            $1 = $0.count
+        })
+    }
+    @inlinable
+    public func fade(out sample: Int, dB: Float64) {
+        fade(prefix: .init(unsafeUninitializedCapacity: sample) {
+            vDSP.formRamp(from: 0, through: 0.05 * M_LOG2E * M_LN10 * dB, result: &$0)
+            vForce.exp2($0, result: &$0)
+            $1 = $0.count
+        })
     }
 }
