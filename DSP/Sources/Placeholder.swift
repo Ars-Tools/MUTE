c4b49f254d271c525bab0b4d5e9166672b9900f3
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-12-01 12:34:52 +0900

    fix

--- DSP/Sources/Placeholder.swift
+++ DSP/Sources/Placeholder.swift
@@ -8,6 +8,7 @@ import typealias Auxiliary.Autorelease
 public enum Placeholder {
     public struct Buffer {
         @usableFromInline let stream: Int
+        @usableFromInline let offset: Int
         @usableFromInline let length: Int
         @usableFromInline let buffer: Autorelease.Memory
     }
@@ -17,14 +18,21 @@ extension Placeholder.Buffer {
     public init(stream: Int, length: Int) {
         self.stream = stream
         self.length = length
+        offset = 0
         buffer = .init(repeating: 0 as Float64, count: stream * length)
+    }
+}
+extension Placeholder.Buffer {
+    @inlinable
+    var memory: UnsafeMutablePointer<Float64> {
+        buffer.start.advanced(by: offset).assumingMemoryBound(to: Float64.self)
     }
 }
 extension Placeholder.Buffer {
     @inlinable
     public func set(source: UnsafePointer<Float64>, stride: Int) {
         DSP.copy(x: source, ldx: stride,
-                 y: buffer.start.assumingMemoryBound(to: Float64.self), ldy: length,
+                 y: memory, ldy: length,
                  rows: stream, cols: length)
     }
 }
@@ -36,9 +44,26 @@ extension Placeholder.Buffer: Stream {
     @inlinable
     public func callAsFunction(interval: CMTime, capacity: Int, instance: inout Instance) throws -> @Sendable (CMTime, Int, UnsafeMutablePointer<Float64>, Int) -> Void {
         {
-            DSP.copy(x: buffer.start.assumingMemoryBound(to: Float64.self), ldx: length,
+            DSP.copy(x: memory, ldx: length,
                      y: $2, ldy: $3,
                      rows: stream, cols: length)
         }
+    }
+}
+extension Placeholder.Buffer: RandomAccessCollection {
+    @inlinable
+    public var startIndex: Int { 0 }
+    @inlinable
+    public var endIndex: Int { count }
+    @inlinable
+    public subscript(position: Int) -> Placeholder.Buffer {
+        self[position...position]
+    }
+    public subscript(bounds: some RangeExpression<Int>) -> Placeholder.Buffer {
+        let bounds = bounds.relative(to: startIndex..<endIndex)
+        return.init(stream: bounds.count,
+                    offset: offset + bounds.lowerBound * MemoryLayout<Float64>.stride,
+                    length: length,
+                    buffer: buffer)
     }
 }
