84d2a793868d70e4cfb1d12f3a98d3a3104d2747
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-08-18 15:48:08 +0900

    Update ESP/Tests/Octave.swift

---
+++ ESP/Tests/Octave.swift
@@ -0,0 +1,19 @@
+//
+//  Octave.swift
+//  MUTE
+//
+//  Created by Kota on 8/18/26.
+//
+import Testing
+import typealias Accelerate.vDSP
+@testable import ESP
+@Suite
+struct Octave {
+    @Test
+    func DeciBelPerOctave() {
+        let (α, β) = `dB/oct.`(frequency: vDSP.ramp(from: 0, through: 16000, count: 128),
+                               magnitude: vDSP.ramp(from: 10, through: 1, count: 128),
+                               bandwidth: 100...10000)
+        print(α, β)
+    }
+}
