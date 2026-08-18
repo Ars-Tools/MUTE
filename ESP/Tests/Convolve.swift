9769233723b5a3e5dac082fd1b2277096deb5b3d
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-08-18 15:48:08 +0900

    Update ESP/Tests/Convolve.swift

---
+++ ESP/Tests/Convolve.swift
@@ -0,0 +1,52 @@
+//
+//  Convolve.swift
+//  MUTE
+//
+//  Created by Kota on 8/6/26.
+//
+import Testing
+import typealias Accelerate.vDSP
+@testable import ESP
+@Suite
+struct ConvolverTestCases {
+    @Test(
+        arguments: [
+            ([1, 2, 2, 2, 2, 2], [1, 0, 2]),
+            ([1, 2, 2, 2, 2, 2], [1, 0, 2, 0, 3]),
+        ]
+    )
+    func naïve(x: Array<Float64>, y: Array<Float64>) {
+        let expect = Array<Float64>(unsafeUninitializedCapacity: x.count + y.count - 1) {
+            vDSP.clear(&$0[0..<$0.count])
+            let (signal, kernel) = x.count < y.count ? (y, x) : (x, y)
+            for (offset, factor) in kernel.enumerated() {
+                vDSP.add(multiplication: (signal, factor), $0[offset..<offset+signal.count], result: &$0[offset..<offset+signal.count])
+            }
+            $1 = $0.count
+        }
+        let convolver = Convolvers.Naïve
+        let result = convolver.convolve(x: x, y: y)
+        #expect(expect.count == result.count)
+        #expect(zip(expect, result).map(-).map(\.magnitude).allSatisfy { $0.isLess(than: .ulpOfOne.squareRoot()) })
+    }
+    @Test(
+        arguments: [
+            ([1, 2, 2, 2, 2, 2], [1, 0, 2]),
+            ([1, 2, 2, 2, 2, 2], [1, 0, 2, 0, 3]),
+        ]
+    )
+    func dft(x: Array<Float64>, y: Array<Float64>) {
+        let expect = Array<Float64>(unsafeUninitializedCapacity: x.count + y.count - 1) {
+            vDSP.clear(&$0[0..<$0.count])
+            let (signal, kernel) = x.count < y.count ? (y, x) : (x, y)
+            for (offset, factor) in kernel.enumerated() {
+                vDSP.add(multiplication: (signal, factor), $0[offset..<offset+signal.count], result: &$0[offset..<offset+signal.count])
+            }
+            $1 = $0.count
+        }
+        let convolver = Convolvers.DFT(count: expect.count)
+        let result = convolver.convolve(x: x, y: y)
+        #expect(expect.count == result.count)
+        #expect(zip(expect, result).map(-).map(\.magnitude).allSatisfy { $0.isLess(than: .ulpOfOne.squareRoot()) })
+    }
+}
