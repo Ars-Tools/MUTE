3664d4f49e78162610c46cdf4050575500f6ab30
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-08-24 19:27:22 +0900

    basical ramp

--- ESP/Sources/Ramp.swift
+++ ESP/Sources/Ramp.swift
@@ -8,12 +8,15 @@ import typealias Accelerate.vDSP
 import typealias Accelerate.vForce
 import func simd.log2
 import func simd.exp2
-@inlinable@_transparent
-public func logspace(in range: ClosedRange<Float64>, count: Int) -> Array<Float64> {
-    .init(unsafeUninitializedCapacity: count) {
-        let range = log2(SIMD2<Float64>(range.lowerBound, range.upperBound))
-        vDSP.formRamp(withInitialValue: range.x, increment: (range.y - range.x) / .init($0.count), result: &$0)
-        vForce.exp2($0, result: &$0)
-        $1 = $0.count
+public enum Ramp {}
+extension Ramp {
+    @inlinable@_transparent
+    public static func logspace(in range: ClosedRange<Float64>, count: Int) -> Array<Float64> {
+        .init(unsafeUninitializedCapacity: count) {
+            let range = log2(SIMD2<Float64>(range.lowerBound, range.upperBound))
+            vDSP.formRamp(withInitialValue: range.x, increment: (range.y - range.x) / .init($0.count), result: &$0)
+            vForce.exp2($0, result: &$0)
+            $1 = $0.count
+        }
     }
 }
