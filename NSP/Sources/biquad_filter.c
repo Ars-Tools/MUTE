32041f1d5110fae72b66ff801b2c55b444e357f4
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-08-25 12:35:50 +0900

    improve biquad filter and header implementation

--- NSP/Sources/biquad_filter.c
+++ NSP/Sources/biquad_filter.c
@@ -8,10 +8,8 @@
 #include"biquad_filter.h"
 __attribute__((always_inline))
 biquad_filter_t * __nonnull const biquad_filter_create(intptr_t const c) {
-	void * const p = __malloc__(sizeof(biquad_filter_t const) + c * sizeof(simd_double4 const));
-	biquad_filter_t * const object = (biquad_filter_t * const)p;
-	*(simd_double4**const)&object->s = (simd_double4*const)(p + sizeof(biquad_filter_t const));
-	*(intptr_t*const)&object->z = c;
+    biquad_filter_t * const object = __malloc__(sizeof(intptr_t const) + c * sizeof(simd_double4 const));
+    *(intptr_t*__nonnull const)object = c;
 	biquad_filter_reset(object);
 	return object;
 }
@@ -21,8 +19,7 @@ void biquad_filter_destroy(biquad_filter_t * __nonnull const object) {
 }
 __attribute__((always_inline))
 void biquad_filter_reset(biquad_filter_t * __nonnull const object) {
-	for ( register intptr_t k = 0, K = *(intptr_t*const)&object->z = object->z ; k < K ; ++ k )
-		object->s[k] = simd_make_double4(0);
+    memset(object->s, 0, object->z * sizeof(simd_double4 const));
 }
 __attribute__((always_inline))
 void biquad_filter_active(biquad_filter_t * __nonnull const object,
@@ -38,37 +35,4 @@ void biquad_filter_active(biquad_filter_t * __nonnull const object,
 									  Y + ldY * c,
 									  object->s + c,
 									  length);
-}
-__attribute__((always_inline))
-inline void biquad_filter_convolve_active(register double const * __nonnull b, register intptr_t const ldb,
-                                          register double const * __nonnull a, register intptr_t const lda,
-                                          register double const * __nonnull x,
-                                          register double       * __nonnull y,
-                                          simd_double4 * __nonnull s,
-                                          intptr_t const length) {
-	register simd_double3 z = (simd_double3 const) {s->x, s->y, 0};
-	register simd_double2 w = (simd_double2 const) {s->z, s->w};
-	for ( register intptr_t k = 0, K = length ; k < K ; ++ k, ++ x, ++ y, ++ b, ++ a )
-		w = (simd_double2 const) {
-			*y = (simd_dot(z = (simd_double3 const) {*x, z.x, z.y}, (simd_double3 const) {*b, b[ldb], b[2*ldb]}) - simd_dot(w, (simd_double2 const) {a[lda], a[2*lda]})) / *a,
-			w.x
-		};
-	*s = (simd_double4 const) {z.x, z.y, w.x, w.y};
-}
-__attribute__((always_inline))
-inline void biquad_filter_convolve_static(register simd_double3 const b,
-                                          register simd_double3 const a,
-                                          register double const * __nonnull x,
-                                          register double       * __nonnull y,
-                                          simd_double4 * __nonnull s,
-                                          intptr_t const length) {
-	register simd_double3 z = (simd_double3 const) {s->x, s->y, 0};
-	register simd_double2 w = (simd_double2 const) {s->z, s->w};
-	register simd_double2 const _ = (simd_double2 const) {a.y, a.z};
-	for ( register intptr_t k = 0, K = length ; k < K ; ++ k, ++ x, ++ y )
-		w = (simd_double2 const) {
-			*y = (simd_dot(z = (simd_double3 const) {*x, z.x, z.y}, b) - simd_dot(w, _)) / a.x,
-			w.x,
-		};
-	*s = (simd_double4 const) {z.x, z.y, w.x, w.y};
 }
--- NSP/Sources/biquad_filter.h
+++ NSP/Sources/biquad_filter.h
@@ -7,8 +7,8 @@
 #include<stdint.h>
 #include<simd/simd.h>
 typedef struct {
-	simd_double4 * __nonnull const s;
 	intptr_t const z;
+    simd_double4 s[1];
 } biquad_filter_t;
 __attribute__((always_inline)) biquad_filter_t * __nonnull const biquad_filter_create(intptr_t const c);
 __attribute__((always_inline)) void biquad_filter_destroy(biquad_filter_t * __nonnull const object);
@@ -19,9 +19,36 @@ __attribute__((always_inline)) void biquad_filter_active(biquad_filter_t * __non
 														 double const * __nonnull X, intptr_t const ldX,
 														 double       * __nonnull Y, intptr_t const ldY,
 														 intptr_t const length);
-__attribute__((always_inline)) inline void biquad_filter_convolve_active(double const * __nonnull b, intptr_t const ldb,
-																		 double const * __nonnull a, intptr_t const lda,
-																		 double const * __nonnull x,
-																		 double       * __nonnull y,
-																		 simd_double4 * __nonnull s,
-																		 intptr_t const length);
+__attribute__((always_inline)) static inline
+void biquad_filter_convolve_active(register double const * __nonnull b, register intptr_t const ldb,
+                                   register double const * __nonnull a, register intptr_t const lda,
+                                   register double const * __nonnull x,
+                                   register double       * __nonnull y,
+                                   simd_double4 * __nonnull s,
+                                   intptr_t const length) {
+    register simd_double3 z = (simd_double3 const) {s->x, s->y, 0};
+    register simd_double2 w = (simd_double2 const) {s->z, s->w};
+    for ( register intptr_t k = 0, K = length ; k < K ; ++ k, ++ x, ++ y, ++ b, ++ a )
+        w = (simd_double2 const) {
+            *y = (simd_dot(z = (simd_double3 const) {*x, z.x, z.y}, (simd_double3 const) {*b, b[ldb], b[2*ldb]}) - simd_dot(w, (simd_double2 const) {a[lda], a[2*lda]})) / *a,
+            w.x
+        };
+    *s = (simd_double4 const) {z.x, z.y, w.x, w.y};
+}
+__attribute__((always_inline)) static inline
+void biquad_filter_convolve_static(register simd_double3 const b,
+                                   register simd_double3 const a,
+                                   register double const * __nonnull x,
+                                   register double       * __nonnull y,
+                                   simd_double4 * __nonnull s,
+                                   intptr_t const length) {
+    register simd_double3 z = (simd_double3 const) {s->x, s->y, 0};
+    register simd_double2 w = (simd_double2 const) {s->z, s->w};
+    register simd_double2 const _ = (simd_double2 const) {a.y, a.z};
+    for ( register intptr_t k = 0, K = length ; k < K ; ++ k, ++ x, ++ y )
+        w = (simd_double2 const) {
+            *y = (simd_dot(z = (simd_double3 const) {*x, z.x, z.y}, b) - simd_dot(w, _)) / a.x,
+            w.x,
+        };
+    *s = (simd_double4 const) {z.x, z.y, w.x, w.y};
+}
