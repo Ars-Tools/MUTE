ab36e9f99c05292941d765dab1ac2c6c7025c9fd
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-08-29 17:29:23 +0900

    octave analysis

--- ESP/Sources/Octave.swift
+++ ESP/Sources/Octave.swift
@@ -5,53 +5,91 @@
 //  Created by Kota on 10/30/25.
 //
 import protocol Accelerate.AccelerateBuffer
-import func LAPACK.gels
 import enum Accelerate.vDSP
 import enum Accelerate.vForce
+import func Darwin.pow
+import let Darwin.M_LN2
 import let Darwin.M_LN10
+import func vFORCE.vvpow
+import func LAPACK.gels
 public enum Octave {}
 extension Octave {
     // estimate slope and intercept for dB/oct. from frequency response
     @inlinable
-    public static func`dB/oct.`(frequency: some AccelerateBuffer<Float64> & Sequence<Float64>,
-                                magnitude: some AccelerateBuffer<Float64>,
-                                bandwidth: some RangeExpression<Float64>) -> (Float64, Float64) {
-        precondition(frequency.count == magnitude.count)
-        let range = frequency.enumerated().compactMap { bandwidth.contains($1) ? .some(UInt($0 + 1)) : .none }
-        let count = range.count
-        let size = gels(count, 2, 1,
-                        unsafeBitCast(Optional<UnsafeMutablePointer<Float64>>.none, to: UnsafeMutablePointer<Float64>.self), count, .N,
-                        unsafeBitCast(Optional<UnsafeMutablePointer<Float64>>.none, to: UnsafeMutablePointer<Float64>.self), count,
-                        .none, -1)
-        assert(0 < size, "lapack error \(size)")
-        return withUnsafeTemporaryAllocation(of: Float64.self, capacity: 2 * count + max(2, count) + size) {
+    public static func`dB/oct.`(frequency: some AccelerateBuffer<Float64>,
+                                response: some AccelerateBuffer<Float64>,
+                                confidence: Optional<some AccelerateBuffer<Float64>> = .none as Optional<Array<Float64>>) -> SIMD2<Float64> {
+        let m = response.count
+        let n = 2
+        let l = gels(m, n, 1,
+                     .none, m, .N,
+                     .none, max(m, n),
+                     .none as Optional<UnsafeMutablePointer<Float64>>, 0)
+        assert(0 < l, "lapack size \(l)")
+        return withUnsafeTemporaryAllocation(of: Float64.self, capacity: m * n + max(m, n) + l) {
+            let A = UnsafeMutableBufferPointer(rebasing: $0.prefix(m * n))
+            let b = UnsafeMutableBufferPointer(rebasing: $0.dropFirst(m * n).prefix(max(m, n)))
+            let w = UnsafeMutableBufferPointer(rebasing: $0.dropFirst(m * n + max(m, n)).prefix(l))
+            // C
+            if let confidence {
+                vForce.sqrt(confidence, result: &A[1*m..<2*m])
+            } else {
+                vDSP.fill(&A[1*m..<2*m], with: 1)
+            }
             // A
-            vDSP.gather(frequency, indices: range, result: &$0[0*count..<1*count])
-            vForce.log2($0[0*count..<1*count], result: &$0[0*count..<1*count])
-            vDSP.fill(&$0[1*count..<2*count], with: 1)
+            vForce.log2(frequency, result: &A[0..<m])
+            vDSP.multiply(A[1*m..<2*m], A[0..<m], result: &A[0..<m])
+            Guard.removeAbnormal(of: &A[0..<m])
             // B
-            vDSP.gather(magnitude, indices: range, result: &$0[2*count..<3*count])
-            vForce.log10($0[2*count..<3*count], result: &$0[2*count..<3*count])
-            vDSP.multiply(20, $0[2*count..<3*count], result: &$0[2*count..<3*count])
-            // solve
-            let info = gels(count, 2, 1,
-                            $0.baseAddress.unsafelyUnwrapped.advanced(by: 0 * count), count, .N,
-                            $0.baseAddress.unsafelyUnwrapped.advanced(by: 2 * count), count,
-                            $0.baseAddress.unsafelyUnwrapped.advanced(by: 2 * count + max(2, count)), size)
-            assert(info == 0, "lapack error \(info)")
-            return ($0[2*count+0], $0[2*count+1])
+            vForce.log10(response, result: &b[0..<m])
+            vDSP.multiply(20, b[0..<m], result: &b[0..<m])
+            vDSP.multiply(A[1*m..<2*m], b[0..<m], result: &b[0..<m])
+            Guard.removeAbnormal(of: &b[0..<m])
+            let s = gels(m, n, 1,
+                         A.baseAddress, m, .N,
+                         b.baseAddress, max(m, n),
+                         w.baseAddress, l)
+            assert(0 == s)
+            return.init(b[0], b[1])
         }
     }
+    @inlinable@inline(__always)@_transparent
+    public static func`dB/oct.`(response: some AccelerateBuffer<Float64>,
+                                confidence: Optional<some AccelerateBuffer<Float64>> = .none as Optional<Array<Float64>>) -> SIMD2<Float64> {
+        withUnsafeTemporaryAllocation(of: Float64.self, capacity: response.count) {
+            vDSP.formRamp(withInitialValue: 0.5, increment: 1, result: &$0[0..<$0.count])
+            vDSP.divide($0, .init($0.count), result: &$0[0..<$0.count])
+            return `dB/oct.`(frequency: $0,
+                             response: response,
+                             confidence: confidence)
+        }
+    }
+}
+extension Octave {
     @inlinable
     public static func lnslope(frequency: some AccelerateBuffer<Float64> & Sequence<Float64>,
-                               magnitude: some AccelerateBuffer<Float64>,
-                               bandwidth: some RangeExpression<Float64>) -> Array<Float64> {
-        let (α, β) = `dB/oct.`(frequency: frequency, magnitude: magnitude, bandwidth: bandwidth)
+                               response: some AccelerateBuffer<Float64>,
+                               confidence: Optional<some AccelerateBuffer<Float64>> = .none as Optional<Array<Float64>>) -> Array<Float64> {
+        let ƒ = `dB/oct.`(frequency: frequency,
+                          response: response,
+                          confidence: confidence) * M_LN10 / 20
         return.init(unsafeUninitializedCapacity: frequency.count) {
             vForce.log2(frequency, result: &$0)
-            vDSP.invertedClip($0, to: 0...0, result: &$0)
-            vDSP.add(multiplication: ($0, 0.05 * M_LN10 * α), 0.05 * M_LN10 * β, result: &$0)
+            vDSP.add(multiplication: ($0, ƒ.x), ƒ.y, result: &$0)
+            vForce.exp($0, result: &$0)
+            Guard.removeAbnormal(of: $0)
             $1 = $0.count
+        }
+    }
+    @inlinable@inline(__always)@_transparent
+    public static func lnslope(response: some AccelerateBuffer<Float64>,
+                               confidence: Optional<some AccelerateBuffer<Float64>> = .none as Optional<Array<Float64>>) -> Array<Float64> {
+        withUnsafeTemporaryAllocation(of: Float64.self, capacity: response.count) {
+            vDSP.formRamp(withInitialValue: 0.5, increment: 1, result: &$0[0..<$0.count])
+            vDSP.divide($0, .init($0.count), result: &$0[0..<$0.count])
+            return lnslope(frequency: $0,
+                           response: response,
+                           confidence: confidence)
         }
     }
 }
--- ESP/Tests/Octave.swift
+++ ESP/Tests/Octave.swift
@@ -10,10 +10,25 @@ import typealias Accelerate.vDSP
 @Suite
 struct OctaveTestCases {
     @Test
+    func threeband() {
+        let cascade = [
+            BiquadFilter.LSF(ω₀: 1/8.0, Q: 2.0.squareRoot(), dB:  6),
+            BiquadFilter.HSF(ω₀: 3/8.0, Q: 2.0.squareRoot(), dB: -6)
+        ] as Array<(Float64, Float64, Float64, Float64, Float64)>
+        let frequency = Ramp.linspace(in: 0.01 ... 0.5, count: 256)
+        let response = TransferFunction.response(frequency: frequency, cascade: cascade)
+//        print(response)
+        let slope = Octave.lnslope(frequency: frequency,
+                                   response: response.map(\.magnitude),
+                                   confidence: frequency.map {
+            (0.0...0.2).contains($0) ? 1 : 0
+        })
+        print(slope)
+    }
+    @Test
     func DeciBelPerOctave() {
-        let (α, β) = Octave.`dB/oct.`(frequency: vDSP.ramp(from: 0, through: 16000, count: 128),
-                                      magnitude: vDSP.ramp(from: 10, through: 1, count: 128),
-                                      bandwidth: 100...10000)
-        print(α, β)
+        let αβ = Octave.`dB/oct.`(frequency: vDSP.ramp(from: 100, through: 16000, count: 128),
+                                  response: vDSP.ramp(from: 10, through: 1, count: 128))
+        print(αβ)
     }
 }
