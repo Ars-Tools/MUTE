63750ed364625ceb70bcef60af01a7ac0e538568
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-09-21 23:04:43 +0900

    cum range

--- DSP/Sources/Extension+Sequence.swift
+++ DSP/Sources/Extension+Sequence.swift
@@ -8,21 +8,21 @@
 @preconcurrency import typealias Combine.Publishers
 import func Layout.concat
 extension Sequence {
-	@inlinable @inline(__always)
+    @inlinable@_transparent
 	func roll(head count: Int) -> some Sequence<Element> {
 		concat(dropFirst(count), prefix(count))
 	}
-	@inlinable @inline(__always)
+    @inlinable@_transparent
 	func roll(tail count: Int) -> some Sequence<Element> {
 		concat(suffix(count), dropLast(count))
 	}
-	@inlinable @inline(__always)
+    @inlinable@_transparent
 	func prefix(count: Int) -> some Publisher<(Int, Element), Never> & Sendable {
 		prefix(count).enumerated().publisher.map(\.self)
 	}
 }
 extension Sequence where Element: Numeric {
-	@inlinable @inline(__always)
+    @inlinable@_transparent
 	public var cumulative: some Sequence<Element> {
 		sequence(state: (makeIterator(), Element.zero)) { state in
 			state.0.next().map { element in
@@ -31,4 +31,15 @@ extension Sequence where Element: Numeric {
 			}
 		}
 	}
+}
+extension Sequence where Element: BinaryInteger {
+    @inlinable@_transparent
+    public var cumulatedRanges: some Sequence<Range<Element>> {
+        sequence(state: (makeIterator(), Element.zero)) { state in
+            state.0.next().map { element in
+                defer { state.1 += element }
+                return state.1..<state.1 + element
+            }
+        }
+    }
 }
