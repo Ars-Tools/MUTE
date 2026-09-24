02925b608121a624a4d3ff455eaae7295e63a04a
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-09-24 16:13:24 +0900

    biquad

--- DSP/Sources/Filter+Biquad.swift
+++ DSP/Sources/Filter+Biquad.swift
@@ -5,6 +5,7 @@
 //  Created by Kota on 7/11/R7.
 //
 @preconcurrency import protocol Combine.Publisher
+@preconcurrency import protocol Accelerate.AccelerateBuffer
 import typealias Synchronization.Mutex
 import typealias Accelerate.vDSP
 import typealias Accelerate.vForce
@@ -15,94 +16,292 @@ import func Accelerate.vecLib.vDSP_biquadm_SetTargetsDoubleD
 import func Accelerate.vecLib.vDSP_biquadm_SetCoefficientsDoubleD
 import func Accelerate.vecLib.vDSP_biquadm_SetActiveFiltersD
 import func Accelerate.vecLib.vDSP_biquadmD
-import func simd.exp10
-import func simd.log2
-import func simd.fma
-import func simd.cos
-import func simd.__sincospi_stret
+import func Accelerate.vecLib.vDSP_biquad_CreateSetupD
+import func Accelerate.vecLib.vDSP_biquad_DestroySetupD
+import func Accelerate.vecLib.vDSP_biquadD
+import Accelerate
 import func NSP.biquad_filter_create
 import func NSP.biquad_filter_destroy
 import func NSP.biquad_filter_active
+import func NSP.biquad_filter_convolve_active
+import func simd.cos
+import func simd.log2
 import typealias Auxiliary.Autorelease
-public enum BiquadFilter {
-	public enum Design: Sendable {
-		case lpf(ω₀: Frequency, quality: Float64) // lo-pass
-		case hpf(ω₀: Frequency, quality: Float64) // hi-pass
-		case bpf(ω₀: Frequency, quality: Float64) // band-pass
-		case bsf(ω₀: Frequency, quality: Float64) // band-stop
-		case apf(ω₀: Frequency, quality: Float64) // all-pass
-		case lsf(ω₀: Frequency, quality: Float64, gain: Float64) // lo-shelf
-		case hsf(ω₀: Frequency, quality: Float64, gain: Float64) // hi-shelf
-		case peq(ω₀: Frequency, quality: Float64, gain: Float64) // peaking
-		case raw(b₀: Float64, b₁: Float64, b₂: Float64, a₁: Float64, a₂: Float64) // raw coefficients
-	}
+import typealias ESP.BiquadFilter
+extension SIMD3: @retroactive RandomAccessCollection {
+    public var startIndex: Int { 0 }
+    public var endIndex: Int { scalarCount }
+}
+extension SIMD3: @retroactive AccelerateBuffer {
+    
+}
+extension Filter {
+    public protocol Biquad<Element>: TransferFunction where B == SIMD3<Element>, A == SIMD3<Element>, Element: SIMDScalar {
+        @inlinable func coefficients(for Tₛ: CMTime) -> (b: B, a: A)
+    }
+}
+extension Filter.Biquad {
+    @inlinable
+    public var counts: SIMD2<Int> {
+        .init(3, 3)
+    }
+    @inlinable
+    public func normalizedCoefficients(for Tₛ: CMTime) -> Array<Element> where Element == Float64 {
+        let (b, a) = coefficients(for: Tₛ)
+        return.init(unsafeUninitializedCapacity: 5) {
+            $1 = $0.startIndex.distance(to: $0[$0.initialize(fromContentsOf: b / a.x)...].initialize(fromContentsOf: (a / a.x).dropFirst()))
+        }
+    }
+    @inlinable
+    public func normalizedCoefficients(for Tₛ: CMTime) -> Array<Element> where Element == Float32 {
+        let (b, a) = coefficients(for: Tₛ)
+        return.init(unsafeUninitializedCapacity: 5) {
+            $1 = $0.startIndex.distance(to: $0[$0.initialize(fromContentsOf: b / a.x)...].initialize(fromContentsOf: (a / a.x).dropFirst()))
+        }
+    }
+    @inlinable
+    public func normalizedCoefficients(for Tₛ: CMTime) -> Array<Element> where Element == Float16 {
+        let (b, a) = coefficients(for: Tₛ)
+        return.init(unsafeUninitializedCapacity: 5) {
+            $1 = $0.startIndex.distance(to: $0[$0.initialize(fromContentsOf: b / a.x)...].initialize(fromContentsOf: (a / a.x).dropFirst()))
+        }
+    }
+}
+extension Filter {
+    @usableFromInline
+    enum SOS {
+        @usableFromInline
+        struct Kr<Signal: Publisher<(Int, (Int, Design)), Never> & Sendable, Design: Filter.Biquad<Float64>> {
+            @usableFromInline let source: Stream
+            @usableFromInline let design: Signal
+            @usableFromInline let stages: Int
+        }
+        @usableFromInline // audio-rate filtering, shared filter coefficients for all channels
+        struct Ar<Batch: Sequence<Stream> & Sendable> {
+            @usableFromInline let source: Stream
+            @usableFromInline let design: Batch // serialized section (b a)
+        }
+    }
+}
+extension Filter.SOS.Kr: DSP.Stream {
+    @inlinable
+    var count: Int {
+        source.count
+    }
+    @inlinable
+    func callAsFunction(interval: CMTime, capacity: Int, instance: inout Instance) throws -> @Sendable (CMTime, Int, UnsafeMutablePointer<Float64>, Int) -> Void {
+        let kernel = try source(interval: interval, capacity: capacity, instance: &instance)
+        switch source.count {
+//        case 1:
+        case let stream:
+            let object = switch vDSP_biquadm_CreateSetupD(repeatElement(Array(arrayLiteral: 1, 0, 0, 0, 0), count: stream * stages).flatMap(\.self), .init(stages), .init(stream)) {
+            case.some(let opaque):
+                Autorelease.Opaque(pointer: opaque, release: vDSP_biquadm_DestroySetupD)
+            case.none:
+                throw Error.failedToAllocate(OpaquePointer.self)
+            }
+            let cancel = design.sink {
+                switch ($0, $1.0) {
+                case (0..<stream, 0..<stages):
+                    vDSP_biquadm_SetCoefficientsDoubleD(object.pointer,
+                                                        $1.1.normalizedCoefficients(for: interval),
+                                                        .init($1.0), .init($0),
+                                                        1, 1)
+                default:
+                    assertionFailure("out of range")
+                }
+            }
+            return { [cancel, object] in
+                kernel($0, $1, $2, $3)
+                var x = stride(from: 0, to: stream * $3, by: $3).map(UnsafePointer($2).advanced(by:))
+                var y = stride(from: 0, to: stream * $3, by: $3).map($2.advanced(by:))
+                vDSP_biquadmD(object.pointer,
+                              &x, 1,
+                              &y, 1,
+                              .init($1))
+            }
+        }
+    }
+}
+extension Filter.SOS.Ar: DSP.Stream {
+    @inlinable
+    var count: Int {
+        source.count
+    }
+    @inlinable
+    func callAsFunction(interval: CMTime, capacity: Int, instance: inout Instance) throws -> @Sendable (CMTime, Int, UnsafeMutablePointer<Float64>, Int) -> Void {
+        switch source.count {
+//        case 1:
+//            let batches = try design.map {
+//                switch $0.count.quotientAndRemainder(dividingBy: 6) {
+//                case (let stage, 0):
+//                    try (Autorelease.Object(object: biquad_filter_create(stage)) {
+//                        biquad_filter_destroy($0)
+//                    }, $0(interval: interval, capacity: capacity, instance: &instance))
+//                default:
+//                    throw Error.unmatchChannel
+//                }
+//            }
+//            let stages = batches.map(\.0.reference.pointee.z).max() ?? 0
+//            let kernel = try source(interval: interval, capacity: capacity, instance: &instance)
+        case let count:
+            let batches = try design.map {
+                switch $0.count.quotientAndRemainder(dividingBy: 6) {
+                case (let stage, 0):
+                    try (Autorelease.Object(object: biquad_filter_create(stage, count)) {
+                        biquad_filter_destroy($0)
+                    }, $0(interval: interval, capacity: capacity, instance: &instance))
+                default:
+                    throw Error.unmatchChannel
+                }
+            }
+            let stages = batches.map(\.0.reference.pointee.order).max() ?? 0
+            let kernel = try source(interval: interval, capacity: capacity, instance: &instance)
+            return { moment, length, target, stride in
+                kernel(moment, length, target, stride)
+                withUnsafeTemporaryAllocation(of: Float64.self, capacity: 6 * stages * length) {
+                    guard case.some(let θ) = $0.baseAddress else {
+                        return assertionFailure()
+                    }
+                    for (object, coefficients) in batches {
+                        coefficients(moment, length, θ, length)
+                        biquad_filter_active(object.reference,
+                                             θ.advanced(by: 0 * length), 6 * length,
+                                             θ.advanced(by: 1 * length), 6 * length,
+                                             θ.advanced(by: 2 * length), 6 * length,
+                                             θ.advanced(by: 3 * length), 6 * length,
+                                             θ.advanced(by: 4 * length), 6 * length,
+                                             θ.advanced(by: 5 * length), 6 * length,
+                                             target, stride,
+                                             target, stride,
+                                             length)
+                    }
+                }
+            }
+        }
+    }
+}
+// MARK: filter functions
+@_disfavoredOverload
+public func filter(_ source: Stream, sos design: some Publisher<(Int, (Int, some Filter.Biquad<Float64>)), Never> & Sendable, count: Int) -> some Stream {
+    Filter.SOS.Kr(source: source, design: design, stages: count)
+}
+@inlinable
+public func filter(_ source: Stream, sos design: some Publisher<(Int, some Filter.Biquad<Float64>), Never>, count: Int) -> some Stream {
+    filter(source, sos: design.repeat(count: source.count), count: count)
+}
+@inlinable
+public func filter(_ source: Stream, sos design: some Publisher<(Int, some Collection<some Filter.Biquad<Float64>>), Never>, count: Int) -> some Stream {
+    filter(source, sos: design.flatMap { (key, value) in value.enumerated().publisher.map { (key, $0) } }, count: count)
+}
+@inlinable
+public func filter(_ source: Stream, sos design: some Publisher<some Collection<some Filter.Biquad<Float64>>, Never>, count: Int) -> some Stream {
+    filter(source, sos: design.repeat(count: source.count), count: count)
+}
+@inlinable
+public func filter(_ source: Stream, sos design: some Collection<some Filter.Biquad<Float64>>) -> some Stream {
+    filter(source, sos: `repeat`(design, count: source.count), count: design.count)
+}
+@inlinable
+public func filter<Design: Filter.Biquad<Float64>>(_ source: Stream, sos design: Design...) -> some Stream {
+    filter(source, sos: design)
+}
+@_disfavoredOverload
+public func filter(_ source: Stream, sos design: some Sequence<Stream> & Sendable) -> some Stream { // 1 batch = [section][b0, b1, b2, a0, a1, a2] layout
+    Filter.SOS.Ar(source: source, design: design)
+}
+@inlinable
+public func filter(_ source: Stream, sos design: Stream...) -> some Stream { // 1 batch = [section][b0, b1, b2, a0, a1, a2] layout
+    filter(source, sos: design)
+}
+
+
+// MARK: Legacy
+extension BiquadFilter {
+    public enum Design: Sendable {
+        case bpf(ω₀: Frequency, quality: Float64) // band-pass
+        case lpf(ω₀: Frequency, quality: Float64) // lo-pass
+        case hpf(ω₀: Frequency, quality: Float64) // hi-pass
+        case apf(ω₀: Frequency, quality: Float64) // all-pass
+        case bsf(ω₀: Frequency, quality: Float64) // band-stop
+        case lsf(ω₀: Frequency, quality: Float64, gain: Float64) // lo-shelf
+        case hsf(ω₀: Frequency, quality: Float64, gain: Float64) // hi-shelf
+        case peq(ω₀: Frequency, quality: Float64, gain: Float64) // peaking
+        case raw(b₀: Float64, b₁: Float64, b₂: Float64, a₁: Float64, a₂: Float64) // raw coefficients
+    }
+}
+extension BiquadFilter.Design {
+    @inlinable@inline(__always)@_transparent
+    public init(lpf: (ω₀: Frequency, Q: Float64)) {
+        self = .lpf(ω₀: lpf.ω₀, quality: lpf.Q)
+    }
+    @inlinable@inline(__always)@_transparent
+    public init(hpf: (ω₀: Frequency, Q: Float64)) {
+        self = .hpf(ω₀: hpf.ω₀, quality: hpf.Q)
+    }
+    @inlinable@inline(__always)@_transparent
+    public init(bpf: (ω₀: Frequency, Q: Float64)) {
+        self = .bpf(ω₀: bpf.ω₀, quality: bpf.Q)
+    }
+    @inlinable@inline(__always)@_transparent
+    public init(bsf: (ω₀: Frequency, Q: Float64)) {
+        self = .bsf(ω₀: bsf.ω₀, quality: bsf.Q)
+    }
+    @inlinable@inline(__always)@_transparent
+    public init(apf: (ω₀: Frequency, Q: Float64)) {
+        self = .apf(ω₀: apf.ω₀, quality: apf.Q)
+    }
+    @inlinable@inline(__always)@_transparent
+    public init(lsf: (ω₀: Frequency, Q: Float64, dB: Float64)) {
+        self = .lsf(ω₀: lsf.ω₀, quality: lsf.Q, gain: lsf.dB)
+    }
+    @inlinable@inline(__always)@_transparent
+    public init(hsf: (ω₀: Frequency, Q: Float64, dB: Float64)) {
+        self = .hsf(ω₀: hsf.ω₀, quality: hsf.Q, gain: hsf.Q)
+    }
+    @inlinable@inline(__always)@_transparent
+    public init(peq: (ω₀: Frequency, Q: Float64, dB: Float64)) {
+        self = .peq(ω₀: peq.ω₀, quality: peq.Q, gain: peq.dB)
+    }
+    @inlinable@inline(__always)@_transparent
+    public init(b₀: Float64, b₁: Float64, b₂: Float64, a₁: Float64, a₂: Float64) {
+        self = .raw(b₀: b₀, b₁: b₁, b₂: b₂, a₁: a₁, a₂: a₁)
+    }
+    @inlinable@inline(__always)@_transparent
+    public init(_ raw: (b₀: Float64, b₁: Float64, b₂: Float64, a₁: Float64, a₂: Float64)) {
+        self.init(b₀: raw.b₀, b₁: raw.b₁, b₂: raw.b₂, a₁: raw.a₁, a₂: raw.a₂)
+    }
+    @inlinable
+    public init(z: (r: Float64, θ: Float64), p: (r: Float64, θ: Float64)) {
+        let x = SIMD2<Float64>(z.r, p.r)
+        let y = 2 * x * cos(SIMD2<Float64>(z.θ, p.θ))
+        let z = x * x
+        self = .raw(b₀: 1, b₁: y.x, b₂: z.x, a₁: y.y, a₂: z.y)
+    }
 }
 extension BiquadFilter.Design {
 	@inlinable@inline(__always)
 	public func coefficients(for Ts: CMTime) -> (b₀: Float64, b₁: Float64, b₂: Float64, a₁: Float64, a₂: Float64) {
 		switch self {
+        case.bpf(let ω₀, let Q):
+            BiquadFilter.BPF(ω₀: ω₀.increment(for: Ts), Q: Q)
 		case.lpf(let ω₀, let Q):
-			let e = __sincospi_stret(2.0 * ω₀.increment(for: Ts))
-			let α = e.__sinval / Q
-			let λ = SIMD3(1 - e.__cosval, -2 * e.__cosval, fma(-0.5, α, 1)) / fma( 0.5, α, 1)
-			return (0.5 * λ.x, λ.x, 0.5 * λ.x, λ.y, λ.z)
+            BiquadFilter.LPF(ω₀: ω₀.increment(for: Ts), Q: Q)
 		case.hpf(let ω₀, let Q):
-			let e = __sincospi_stret(2.0 * ω₀.increment(for: Ts))
-			let α = e.__sinval / Q
-			let λ = SIMD3(1 + e.__cosval, -2 * e.__cosval, fma(-0.5, α, 1)) / fma( 0.5, α, 1)
-			return (0.5 * λ.x,-λ.x, 0.5 * λ.x, λ.y, λ.z)
-		case.bpf(let ω₀, let Q):
-			let e = __sincospi_stret(2.0 * ω₀.increment(for: Ts))
-			let α = e.__sinval / Q
-			let λ = SIMD3(0.5 * α, -2 * e.__cosval, fma(-0.5, α, 1)) / fma( 0.5, α, 1)
-			return (λ.x, 0, -λ.x, λ.y, λ.z)
+            BiquadFilter.HPF(ω₀: ω₀.increment(for: Ts), Q: Q)
+        case.apf(let ω₀, let Q):
+            BiquadFilter.APF(ω₀: ω₀.increment(for: Ts), Q: Q)
 		case.bsf(let ω₀, let Q):
-			let e = __sincospi_stret(2.0 * ω₀.increment(for: Ts))
-			let α = e.__sinval / Q
-			let λ = SIMD3(1, -2 * e.__cosval, fma(-0.5, α, 1)) / fma( 0.5, α, 1)
-			return (λ.x, λ.y, λ.x, λ.y, λ.z)
-		case.apf(let ω₀, let Q):
-			let e = __sincospi_stret(2.0 * ω₀.increment(for: Ts))
-			let s = e.__sinval / Q
-			let λ = SIMD2(-2 * e.__cosval, fma(-0.5, s, 1)) / fma(0.5, s, 1)
-			return (λ.y, λ.x, 1, λ.x, λ.y)
+            BiquadFilter.BSF(ω₀: ω₀.increment(for: Ts), Q: Q)
 		case.lsf(let ω₀, let Q, let dB):
-			let A = exp10(dB * SIMD2<Float64>(0.0125, 0.025))
-			let e = __sincospi_stret(2.0 * ω₀.increment(for: Ts))
-			let μ = SIMD2(repeating: A.y) + SIMD2(-1, 1)
-			let λ = fma(SIMD2(-e.__sinval, e.__sinval), SIMD2(repeating: A.x/Q), SIMD2(repeating: μ.y))
-			let α = SIMD3(μ.x, μ.y, μ.x)
-			let β = SIMD3(λ.y, μ.x, λ.x)
-			let a = fma(SIMD3(repeating:  e.__cosval), α, β)
-			let b = fma(SIMD3(repeating: -e.__cosval), α, β) * A.y / a.x
-			return (b.x, 2 * b.y, b.z, -2 * a.y / a.x, a.z / a.x)
+            BiquadFilter.LSF(ω₀: ω₀.increment(for: Ts), Q: Q, dB: dB)
 		case.hsf(let ω₀, let Q, let dB):
-			let A = exp10(dB * SIMD2<Float64>(0.0125, 0.025))
-			let e = __sincospi_stret(2.0 * ω₀.increment(for: Ts))
-			let μ = SIMD2(repeating: A.y) + SIMD2(-1, 1)
-			let λ = fma(SIMD2(-e.__sinval, e.__sinval), SIMD2(repeating: A.x/Q), SIMD2(repeating: μ.y))
-			let α = SIMD3(μ.x, μ.y, μ.x)
-			let β = SIMD3(λ.y, μ.x, λ.x)
-			let a = fma(SIMD3(repeating: -e.__cosval), α, β)
-			let b = fma(SIMD3(repeating:  e.__cosval), α, β) * A.y / a.x
-			return (b.x, -2 * b.y, b.z, 2 * a.y / a.x, a.z / a.x)
+            BiquadFilter.HSF(ω₀: ω₀.increment(for: Ts), Q: Q, dB: dB)
 		case.peq(let ω₀, let Q, let dB):
-			let A = exp10(dB * SIMD2<Float64>(-0.025, 0.025))
-			let e = __sincospi_stret(2.0 * ω₀.increment(for: Ts))
-			let α = fma(SIMD2(-e.__sinval, e.__sinval), SIMD2(repeating: A.x/Q), SIMD2(repeating: 2))
-			let β = fma(SIMD2(-e.__sinval, e.__sinval), SIMD2(repeating: A.y/Q), SIMD2(repeating: 2))
-			let λ = SIMD4(β.y, β.x, -4 * e.__cosval, α.x) / α.y
-			return (λ.x, λ.z, λ.y, λ.z, λ.w)
+            BiquadFilter.PEQ(ω₀: ω₀.increment(for: Ts), Q: Q, dB: dB)
 		case.raw(let b₀, let b₁, let b₂, let a₁, let a₂):
-			return (b₀, b₁, b₂, a₁, a₂)
+			(b₀, b₁, b₂, a₁, a₂)
 		}
-	}
-	@inlinable @inline(__always)
-	public init(zero z: (r: Float64, θ: Float64), pole p: (r: Float64, θ: Float64)) {
-		let x = SIMD2<Float64>(z.r, p.r)
-		let y = 2 * x * cos(SIMD2<Float64>(z.θ, p.θ))
-		let z = x * x
-		self = .raw(b₀: 1, b₁: y.x, b₂: z.x, a₁: y.y, a₂: z.y)
 	}
 }
 extension BiquadFilter {
