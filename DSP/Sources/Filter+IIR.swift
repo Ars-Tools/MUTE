ce05ff2c474fe41923d4fb735414c0b17f01c09d
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-09-24 20:47:43 +0900

    iir

--- DSP/Sources/Filter+IIR.swift
+++ DSP/Sources/Filter+IIR.swift
@@ -4,112 +4,178 @@
 //
 //  Created by Kota on 7/14/R7.
 //
+@preconcurrency import protocol Accelerate.AccelerateBuffer
 @preconcurrency import protocol Combine.Publisher
-import func NSP.universal_filter_create
-import func NSP.universal_filter_destroy
-import func NSP.universal_filter_active
-import func NSP.universal_filter_static
+import func NSP.transversal_filter_create
+import func NSP.transversal_filter_destroy
+import func NSP.transversal_filter_active
+import func NSP.transversal_filter_static
 import typealias Synchronization.Mutex
 import func simd.simd_max
 import typealias Auxiliary.Autorelease
-@usableFromInline
-enum IIR {
-	@usableFromInline
-	struct Kr<Signal: Publisher<(Int, (Zero, Pole)), Never> & Sendable, Zero: Kernel<Float64>, Pole: Kernel<Float64>> {
-		@usableFromInline let x: Stream
-		@usableFromInline let z: Signal
-		@usableFromInline let b: Int
-		@usableFromInline let a: Int
-	}
-	@usableFromInline
-	struct Ar {
-		@usableFromInline let x: Stream
-		@usableFromInline let b: Stream
-		@usableFromInline let a: Stream
-	}
+extension Filter {
+    @usableFromInline
+    enum IIR {
+        @usableFromInline
+        struct Kr<Signal: Publisher<(Int, Design), Never> & Sendable, Design: Filter.TransferFunction<Float64>> {
+            @usableFromInline let x: Stream
+            @usableFromInline let z: Signal
+            @usableFromInline let b: Int
+            @usableFromInline let a: Int
+        }
+        @usableFromInline
+        struct Ar {
+            @usableFromInline let x: Stream
+            @usableFromInline let b: Stream
+            @usableFromInline let a: Stream
+        }
+    }
 }
-extension IIR.Kr: Stream {
-	@inlinable @inline(__always)
+extension Filter.IIR.Kr: Stream {
+	@inlinable
 	var count: Int {
 		x.count
 	}
 	@inlinable
 	func callAsFunction(interval: CMTime, capacity: Int, instance: inout Instance) throws -> @Sendable (CMTime, Int, UnsafeMutablePointer<Float64>, Int) -> Void {
 		let xk = try x(interval: interval, capacity: capacity, instance: &instance)
-		let c = x.count
-		let object = Autorelease.Opaque(pointer: universal_filter_create(b, a, c), release: universal_filter_destroy)
-		let system = Mutex<(Array<Float64>, Array<Float64>)>((.init(repeating: 0, count: c * b), .init(repeating: 0, count: c * a)))
-		let cancel = z.sink { k, zp in
-			switch k {
-			case 0..<c:
-				system.withLock {
-					$0.0.replaceSubrange(b*k..<b*k+zp.0.count, with: zp.0.coefficients(for: interval))
-					$0.1.replaceSubrange(a*k..<a*k+zp.1.count, with: zp.1.coefficients(for: interval))
-				}
-			default:
-				assertionFailure("out of range")
-			}
-		}
-		return {
-			xk($0, $1, $2, $3)
-			let (B, A) = withExtendedLifetime(cancel) { system.withLock(\.self) }
-			universal_filter_static(object.pointer,
-									B, b,
-									A, a,
-									$2, $3,
-									$2, $3,
-									$1)
-		}
+        switch x.count {
+        case 1:
+            let system = Mutex<(Array<Float64>, Array<Float64>)>((
+                .init(repeating: 0, count: b),
+                .init(repeating: 0, count: a))
+            )
+            let cancel = z.sink { k, zp in
+                switch k {
+                case 0:
+                    let (B, A) = zp.coefficients(for: interval)
+                    system.withLock {
+                        $0.0.replaceSubrange(0..<B.count, with: B)
+                        $0.1.replaceSubrange(0..<A.count, with: A)
+                    }
+                default:
+                    assertionFailure("out of range")
+                }
+            }
+            let object = Autorelease.Object(object: transversal_filter_create(b, a)) {
+                transversal_filter_destroy($0)
+            }
+            return { [cancel, object] in
+                xk($0, $1, $2, $3)
+                let (B, A) = system.withLock(\.self)
+                transversal_filter_static(object.reference,
+                                          B,
+                                          A,
+                                          $2,
+                                          $2,
+                                          $1)
+            }
+        case let c:
+            assert(1 < c)
+            let system = Mutex<(Array<Float64>, Array<Float64>)>((
+                .init(repeating: 0, count: b * c),
+                .init(repeating: 0, count: a * c))
+            )
+            let cancel = z.sink { k, zp in
+                switch k {
+                case 0..<c:
+                    let (B, A) = zp.coefficients(for: interval)
+                    system.withLock {
+                        $0.0.replaceSubrange(b*k..<b*k+B.count, with: B)
+                        $0.1.replaceSubrange(a*k..<a*k+A.count, with: A)
+                    }
+                default:
+                    assertionFailure("out of range")
+                }
+            }
+            let object = Autorelease.Object(object: transversal_filter_create(b, a, c)) {
+                transversal_filter_destroy($0)
+            }
+            return { [object, cancel, b, a] in
+                xk($0, $1, $2, $3)
+                let (B, A) = system.withLock(\.self)
+                transversal_filter_static(object.reference,
+                                          B, b,
+                                          A, a,
+                                          $2, $3,
+                                          $2, $3,
+                                          $1)
+            }
+        }
 	}
 }
-extension IIR.Ar: Stream {
+extension Filter.IIR.Ar: Stream {
 	@inlinable @inline(__always)
 	var count: Int {
 		x.count
 	}
 	@inlinable @inline(__always)
 	func callAsFunction(interval: CMTime, capacity: Int, instance: inout Instance) throws -> @Sendable (CMTime, Int, UnsafeMutablePointer<Float64>, Int) -> Void {
-		let xc = x.count
 		let bc = b.count
 		let ac = a.count
-		let xk = try x(interval: interval, capacity: capacity, instance: &instance)
 		let bk = try b(interval: interval, capacity: capacity, instance: &instance)
 		let ak = try a(interval: interval, capacity: capacity, instance: &instance)
-		let object = Autorelease.Opaque(pointer: universal_filter_create(bc, ac, xc), release: universal_filter_destroy)
-		return { moment, length, target, stride in
-			withUnsafeTemporaryAllocation(of: Float64.self, capacity: ( bc + ac ) * length) {
-				guard let b = $0.baseAddress else { return }
-				let a = b.advanced(by: bc * length)
-				xk(moment, length, target, stride)
-				bk(moment, length, b, length)
-				ak(moment, length, a, length)
-				universal_filter_active(object.pointer,
-										b, length,
-										a, length,
-										target, stride,
-										target, stride,
-										length)
-			}
-		}
+        let xk = try x(interval: interval, capacity: capacity, instance: &instance)
+        switch x.count {
+        case 1:
+            let object = Autorelease.Object(object: transversal_filter_create(bc, ac)) {
+                transversal_filter_destroy($0)
+            }
+            return { [object] moment, length, target, stride in
+                withUnsafeTemporaryAllocation(of: Float64.self, capacity: ( bc + ac ) * length) {
+                    let b = UnsafeMutableBufferPointer(rebasing: $0.prefix(bc * length))
+                    let a = UnsafeMutableBufferPointer(rebasing: $0.suffix(ac * length))
+                    xk(moment, length, target, stride)
+                    bk(moment, length, b.baseAddress.unsafelyUnwrapped, length)
+                    ak(moment, length, a.baseAddress.unsafelyUnwrapped, length)
+                    transversal_filter_active(object.reference,
+                                              b.baseAddress.unsafelyUnwrapped, length,
+                                              a.baseAddress.unsafelyUnwrapped, length,
+                                              target,
+                                              target,
+                                              length)
+                }
+            }
+        case let xc:
+            assert(1 < xc)
+            let object = Autorelease.Object(object: transversal_filter_create(bc, ac, xc)) {
+                transversal_filter_destroy($0)
+            }
+            return { [object] moment, length, target, stride in
+                withUnsafeTemporaryAllocation(of: Float64.self, capacity: ( bc + ac ) * length) {
+                    guard let b = $0.baseAddress else { return }
+                    let a = b.advanced(by: bc * length)
+                    xk(moment, length, target, stride)
+                    bk(moment, length, b, length)
+                    ak(moment, length, a, length)
+                    transversal_filter_active(object.reference,
+                                              b, length,
+                                              a, length,
+                                              target, stride,
+                                              target, stride,
+                                              length)
+                }
+            }
+        }
 	}
 }
-public func filter(_ source: Stream, with kernel: some Publisher<(Int, (some Kernel<Float64>, some Kernel<Float64>)), Never> & Sendable, length: SIMD2<Int>) -> some Stream {
-	IIR.Kr(x: source, z: kernel, b: length.x, a: length.y)
+@_disfavoredOverload
+public func filter(_ source: Stream, iir design: some Publisher<(Int, some Filter.TransferFunction<Float64>), Never> & Sendable, counts: SIMD2<Int>) -> some Stream {
+    Filter.IIR.Kr(x: source, z: design, b: counts.x, a: counts.y)
 }
-public func filter(_ source: Stream, with kernel: some Publisher<(some Kernel<Float64>, some Kernel<Float64>), Never>, length: SIMD2<Int>) -> some Stream {
-	filter(source, with: kernel.repeat(count: source.count), length: length)
+@inlinable
+public func filter(_ source: Stream, iir design: some Publisher<some Filter.TransferFunction<Float64>, Never>, counts: SIMD2<Int>) -> some Stream {
+	filter(source, iir: design.repeat(count: source.count), counts: counts)
 }
-public func filter(_ source: Stream, with kernel: some Sequence<(some Kernel<Float64>, some Kernel<Float64>)>) -> some Stream{
-	filter(source, with: kernel.prefix(count: source.count), length: kernel.reduce(SIMD2<Int>(repeating: 0)) {
-		simd_max($0, .init($1.0.count, $1.1.count))
-	})
+@inlinable
+public func filter(_ source: Stream, iir design: some Sequence<some Filter.TransferFunction<Float64>>) -> some Stream{
+    filter(source, iir: design.prefix(count: source.count), counts: design.map(\.counts).reduce(SIMD2<Int>(repeating: 0), simd_max))
 }
-public func filter(_ source: Stream, with kernel: (some Kernel<Float64>, some Kernel<Float64>)) -> some Stream {
-	filter(source, with: `repeat`(kernel, count: source.count), length: .init(kernel.0.count, kernel.1.count))
+@inlinable
+public func filter(_ source: Stream, iir design: some Filter.TransferFunction<Float64>) -> some Stream {
+    filter(source, iir: `repeat`(design, count: source.count), counts: design.counts)
 }
-public func filter(_ source: Stream, with kernel: (Stream, Stream)) -> some Stream {
-	IIR.Ar(x: source, b: kernel.0, a: kernel.1)
-}
-public func filter(_ source: Stream, with kernel: Stream) -> some Stream {
-	filter(source, with: (kernel, const(1)))
+@_disfavoredOverload
+public func filter(_ source: Stream, iir kernel: (Stream, Stream)) -> some Stream {
+    Filter.IIR.Ar(x: source, b: kernel.0, a: kernel.1)
 }
