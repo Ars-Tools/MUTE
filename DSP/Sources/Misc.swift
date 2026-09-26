48eeffc048bcc41f15314bd36cde89344106afbc
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-09-27 01:20:51 +0900

    some update

--- DSP/Sources/Misc.swift
+++ DSP/Sources/Misc.swift
@@ -12,44 +12,71 @@ import func Accelerate.vDSP_vclrD
 func copy(x: UnsafePointer<Float64>, ldx: Int,
 		  y: UnsafeMutablePointer<Float64>, ldy: Int,
 		  rows: Int, cols: Int) {
-//	for row in 0..<rows {
-//		memmove(y.advanced(by: row * ldy), x.advanced(by: row * ldx), cols * MemoryLayout<Float64>.stride)
-//	}
 	vDSP_mmovD(x, y, .init(cols), .init(rows), .init(ldx), .init(ldy))
+}
+@inlinable@inline(__always)@_transparent
+func move(x: UnsafePointer<Float64>, ldx: Int,
+          y: UnsafeMutablePointer<Float64>, ldy: Int,
+          rows: Int, cols: Int) {
+    for (x, y) in zip(stride(from: 0, to: rows * ldx, by: ldx).lazy.map(x.advanced(by:)),
+                      stride(from: 0, to: rows * ldy, by: ldy).lazy.map(y.advanced(by:))) {
+        memmove(y, x, cols * MemoryLayout<Float64>.stride)
+    }
 }
 @inlinable@inline(__always)@_transparent
 func zero(x: UnsafeMutablePointer<Float64>, ldx: Int,
           rows: Int, cols: Int) {
-    for r in stride(from: x, to: x.advanced(by: rows * ldx), by: ldx) {
-        vDSP_vclrD(r, 1, .init(cols))
+    for x in stride(from: x, to: x.advanced(by: rows * ldx), by: ldx) {
+        vDSP_vclrD(x, 1, .init(cols))
     }
 }
 @inlinable@inline(__always)@_transparent
 func each(count: Int, closure: (Int) -> Void) {
-	withoutActuallyEscaping(closure) {
-		DispatchQueue.concurrentPerform(iterations: count, execute: unsafeBitCast($0 as (Int) -> Void, to: (@Sendable(Int) -> Void).self))
-	}
-//	(0..<count).forEach(closure)
+    withoutActuallyEscaping(closure) {
+        DispatchQueue.concurrentPerform(iterations: count, execute: unsafeBitCast($0 as (Int) -> Void, to: (@Sendable(Int) -> Void).self))
+    }
 }
 @inlinable@inline(__always)@_transparent
 func each<C: Collection>(element: C, closure: (C.Element) -> Void) where C.Index: Strideable, C.Index.Stride == Int {
 	each(count: element.count) { closure(element[element.startIndex.advanced(by: $0)]) }
 }
-@inlinable@inline(__always)@_transparent
+@inlinable@inline(__always)@_transparent@_disfavoredOverload
 func fold<T>(start: UnsafePointer<T>, count: Int, stream: Int, period: Int) -> Array<UnsafeBufferPointer<T>> {
-	zip(stride(from: 0, to: stream * period, by: period).lazy.map(start.advanced(by:)), repeatElement(count, count: stream)).map(UnsafeBufferPointer<T>.init(start:count:))
+    zip(stride(from: start, to: start.advanced(by: stream * period), by: period), repeatElement(count, count: stream))
+        .map(UnsafeBufferPointer<T>.init(start:count:))
 }
 @inlinable@inline(__always)@_transparent
+func fold<T>(start: UnsafePointer<T>, count: Int, stream: Int, period: Int) -> LazyMapSequence<Zip2Sequence<StrideTo<UnsafePointer<T>>, Repeated<Int>>, UnsafeBufferPointer<T>> {
+    zip(stride(from: start, to: start.advanced(by: stream * period), by: period), repeatElement(count, count: stream)).lazy
+        .map(UnsafeBufferPointer<T>.init(start:count:))
+}
+@inlinable@inline(__always)@_transparent@_disfavoredOverload
 func fold<T>(start: UnsafeMutablePointer<T>, count: Int, stream: Int, period: Int) -> Array<UnsafeMutableBufferPointer<T>> {
-	zip(stride(from: 0, to: stream * period, by: period).lazy.map(start.advanced(by:)), repeatElement(count, count: stream)).map(UnsafeMutableBufferPointer<T>.init(start:count:))
+    zip(stride(from: start, to: start.advanced(by: stream * period), by: period), repeatElement(count, count: stream))
+        .map(UnsafeMutableBufferPointer<T>.init(start:count:))
 }
 @inlinable@inline(__always)@_transparent
+func fold<T>(start: UnsafeMutablePointer<T>, count: Int, stream: Int, period: Int) -> LazyMapSequence<Zip2Sequence<StrideTo<UnsafeMutablePointer<T>>, Repeated<Int>>, UnsafeMutableBufferPointer<T>> {
+    zip(stride(from: start, to: start.advanced(by: stream * period), by: period), repeatElement(count, count: stream)).lazy
+        .map(UnsafeMutableBufferPointer<T>.init(start:count:))
+}
+@inlinable@inline(__always)@_transparent@_disfavoredOverload
 func fold<T>(start: UnsafeMutableBufferPointer<T>, count: Int, stream: Int, period: Int) -> Array<UnsafeMutableBufferPointer<T>> {
-	stride(from: 0, to: stream * period, by: period).map { start.extracting($0..<$0+count) }
+    stride(from: 0, to: stream * period, by: period)
+        .map { start.extracting($0..<$0+count) }
 }
 @inlinable@inline(__always)@_transparent
+func fold<T>(start: UnsafeMutableBufferPointer<T>, count: Int, stream: Int, period: Int) -> LazyMapSequence<StrideTo<Int>, UnsafeMutableBufferPointer<T>> {
+    stride(from: 0, to: stream * period, by: period).lazy
+        .map { start.extracting($0..<$0+count) }
+}
+@inlinable@inline(__always)@_transparent@_disfavoredOverload
 func fold(target: Buffer) -> Array<UnsafeMutableBufferPointer<Float64>> {
 	fold(start: target.start, count: target.period, stream: target.stream, period: target.period)
+}
+@inlinable@inline(__always)@_transparent
+func fold(target: Buffer) -> LazyMapSequence<Zip2Sequence<StrideTo<UnsafePointer<Float64>>, Repeated<Int>>, UnsafeBufferPointer<Float64>> {
+    fold(start: target.start, count: target.period, stream: target.stream, period: target.period)
 }
 @usableFromInline
 @dynamicMemberLookup
@@ -62,9 +89,11 @@ struct SendableContainer<RawValue>: @unchecked Sendable, RawRepresentable {
 	}
 }
 extension SendableContainer {
+    @inlinable
 	subscript<R>(dynamicMember dynamicMember: KeyPath<RawValue, R>) -> R {
 		rawValue[keyPath: dynamicMember]
 	}
+    @inlinable
 	subscript<R>(dynamicMember dynamicMember: ReferenceWritableKeyPath<RawValue, R>) -> R {
 		_read {
 			yield rawValue[keyPath: dynamicMember]
